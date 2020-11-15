93565cc0fbc0650433d27c898fea313dd4e34be1
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-11-15 12:19:11 +0800

    Create 06_逻辑斯蒂回归.py

---
+++ 刘二大人/06_逻辑斯蒂回归.py
@@ -0,0 +1,30 @@
+### 1. Prepare data
+x_data = torch.tensor([[1.0], [2.0], [3.0]])
+y_data = torch.tensor([[2.0], [4.0], [6.0]])
+
+### 2. Design model and using Class
+class LogisticModel(torch.nn.Module):
+    def __init__(self):
+        super(LogisticModel, self).__init__()
+        self.linear = torch.nn.Linear(1, 1)
+
+    def forward(self, x):
+        y_pred = torch.sigmoid(self.linear(x))
+        return y_pred
+
+model = LogisticModel()
+
+### 3. Construct loss and optimizer
+criterion = torch.nn.BCELoss(reduction='sum')
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+
+### 4. Training cycle
+
+for epoch in range(1000):
+    y_pred = model(x_data)
+    loss = criterion(y_pred, y_data)
+    print('epoch: ', epoch, 'lose: ', loss.item())
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
